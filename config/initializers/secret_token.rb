3adbfa0e8ac957f8c5114c17bf141690ccdf993d
Nicholas Bundy <nicholasjaynovak@gmail.com>
2014-02-19 20:19:22 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Recispeak::Application.config.secret_key_base = 'dff9ec76e5b2a18216c6c763fa32e2b6dc541f4510454aca2d55cb67253cb60b4a30abc67ad5dd376825c434b541c2ed9494f27f97ba89151d783f39459b50a0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Recispeak::Application.config.session_store :cookie_store, key: '_recispeak_session'

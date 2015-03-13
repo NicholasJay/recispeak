a7b2c2cbcd6a8a861fb1d7e2941e8e16b0056c3f
Nicholas Bundy <nicholasjaynovak@gmail.com>
2015-03-12 23:52:53 -0400

    new welcome page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,18 @@ class WelcomeController < ApplicationController
   before_action :logged_in?
   
   def index
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      session["user_id"] = @user.id
+      redirect_to user_path(@user)
+    else
+      render :new
+    end
   end
 
 end
